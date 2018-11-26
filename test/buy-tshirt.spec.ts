946832bac2bcc8a95d1fdf61cba84f71ed059b4e
Edwin Mantilla <hackpro.ems@gmail.com>
2018-11-26 12:13:31 -0500

    Separate test in different describe

--- test/buy-tshirt.spec.ts
+++ test/buy-tshirt.spec.ts
@@ -1,33 +1,67 @@
 import { browser } from 'protractor';
-import { MenuContentPage, ProductListPage, ProductAddedModalPage,
-  SummaryStepPage, SignInStepPage, AddressStepPage, ShippingStepPage,
-  PaymentStepPage, BankPaymentStepPage, OrderSumaryPage } from '../src/page';
+import {
+  MenuContentPage,
+  ProductListPage,
+  ProductAddedModalPage,
+  SummaryStepPage,
+  SignInStepPage,
+  AddressStepPage,
+  ShippingStepPage,
+  PaymentStepPage,
+  BankPaymentStepPage,
+  OrderSumaryPage
+} from '../src/page';
 
-describe('Buy a t-shirt', () => {
-  const menuContentPage: MenuContentPage = new MenuContentPage();
-  const productListPage: ProductListPage = new ProductListPage();
-  const productAddedModalPage: ProductAddedModalPage = new ProductAddedModalPage();
-  const summaryStepPage: SummaryStepPage = new SummaryStepPage();
-  const signInStepPage: SignInStepPage = new SignInStepPage();
-  const addressStepPage: AddressStepPage = new AddressStepPage();
-  const shippingStepPage: ShippingStepPage = new ShippingStepPage();
-  const paymentStepPage: PaymentStepPage = new PaymentStepPage();
-  const bankPaymentStepPage: BankPaymentStepPage = new BankPaymentStepPage();
-  const orderSumaryPage: OrderSumaryPage = new OrderSumaryPage();
-
-  it('then should be bought a t-shirt', async () => {
+describe('Given a shopping page', () => {
+  beforeAll(async () => {
     await browser.get('http://automationpractice.com/');
-    await menuContentPage.goToTShirtMenu();
-    await productListPage.addToCar();
-    await productAddedModalPage.proceedToCheckout();
-    await summaryStepPage.proceedToCheckout();
-    await signInStepPage.signIn('aperdomobo@gmail.com', 'WorkshopProtractor');
-    await addressStepPage.proceedToCheckout();
-    await shippingStepPage.agreeTermOfService();
-    await shippingStepPage.proceedToCheckout();
-    await paymentStepPage.payByBankWire();
-    await bankPaymentStepPage.confirmOrder();
-    await expect(orderSumaryPage.getMessageOrderComplete())
-      .toBe('Your order on My Store is complete.');
+  });
+
+  describe('when want to buy a T shirt', () => {
+    beforeAll(async () => {
+      const menuContentPage: MenuContentPage = new MenuContentPage();
+      const productListPage: ProductListPage = new ProductListPage();
+      const productAddedModalPage: ProductAddedModalPage = new ProductAddedModalPage();
+      const summaryStepPage: SummaryStepPage = new SummaryStepPage();
+
+      await menuContentPage.goToTShirtMenu();
+      await productListPage.addToCar();
+      await productAddedModalPage.proceedToCheckout();
+      await summaryStepPage.proceedToCheckout();
+    });
+
+    describe('and login to the application', () => {
+      beforeAll(async () => {
+        const signInStepPage: SignInStepPage = new SignInStepPage();
+        await signInStepPage.signIn('aperdomobo@gmail.com', 'WorkshopProtractor');
+      });
+
+      describe('and select default address', () => {
+        beforeAll(async () => {
+          const addressStepPage: AddressStepPage = new AddressStepPage();
+          await addressStepPage.proceedToCheckout();
+        });
+
+        describe('and pay to the bank', () => {
+          beforeAll(async () => {
+            const shippingStepPage: ShippingStepPage = new ShippingStepPage();
+            const paymentStepPage: PaymentStepPage = new PaymentStepPage();
+            const bankPaymentStepPage: BankPaymentStepPage = new BankPaymentStepPage();
+
+            await shippingStepPage.agreeTermOfService();
+            await shippingStepPage.proceedToCheckout();
+            await paymentStepPage.payByBankWire();
+            await bankPaymentStepPage.confirmOrder();
+          });
+
+          it('then the order should be completed', async () => {
+            const orderSumaryPage: OrderSumaryPage = new OrderSumaryPage();
+
+            await expect(orderSumaryPage.getMessageOrderComplete())
+              .toBe('Your order on My Store is complete.');
+          });
+        });
+      });
+    });
   });
 });
