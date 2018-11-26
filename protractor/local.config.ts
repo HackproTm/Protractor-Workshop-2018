a15f9466ec81efb0c8c5051aeff9b74d107736cf
Edwin Mantilla <hackpro.ems@gmail.com>
2018-11-26 14:24:04 -0500

    Using Capabilities to configure Chrome

--- protractor/local.config.ts
+++ protractor/local.config.ts
@@ -5,6 +5,13 @@ export const config: Config = {
   framework: 'jasmine',
   specs: ['../test/**/*.spec.js'],
   SELENIUM_PROMISE_MANAGER: false,
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['--disable-popup-blocking', '--no-default-browser-check', '--window-size=800,600'],
+      prefs: { credentials_enable_service: false }
+    }
+  },
   getPageTimeout: 30000,
   jasmineNodeOpts: {
     defaultTimeoutInterval: 120000
