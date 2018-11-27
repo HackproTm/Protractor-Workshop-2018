64e68b329b1b443d9d3bd60db40337c009926c13
Edwin Mantilla <hackpro.ems@gmail.com>
2018-11-27 17:16:13 -0500

    Use Zalenium

---
+++ protractor/zalenium.config.ts
@@ -0,0 +1,46 @@
+import { Config, browser } from 'protractor';
+import { reporter } from './helpers/reporter';
+
+const firefoxConfig = {
+  browserName: 'firefox',
+  platform: 'linux',
+  name: 'firefox-tests',
+  shardTestFiles: true,
+  maxInstances: 1
+};
+
+const chromeConfig = {
+  browserName: 'chrome',
+  name: 'chrome-tests',
+  shardTestFiles: true,
+  maxInstances: 1
+};
+
+const multiCapabilities = [chromeConfig, firefoxConfig];
+
+export const config: Config = {
+  multiCapabilities,
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  framework: 'jasmine',
+  specs: ['../test/**/*.spec.js'],
+  SELENIUM_PROMISE_MANAGER: false,
+  capabilities: {
+    name: 'UI Workshop',
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['--disable-popup-blocking', '--no-default-browser-check', '--window-size=800,600'],
+      prefs: { credentials_enable_service: false }
+    }
+  },
+  getPageTimeout: 30000,
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 120000
+  },
+  sauceUser: process.env.SAUCE_USERNAME,
+  sauceKey: process.env.SAUCE_ACCESS_KEY,
+  onPrepare: () => {
+    browser.ignoreSynchronization = true;
+    browser.manage().timeouts().implicitlyWait(3000);
+    reporter();
+  }
+};
