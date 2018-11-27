d13b6209bc8f5e46c3d8c5be95f3cb6a54638d7f
Edwin Mantilla <hackpro.ems@gmail.com>
2018-11-27 15:43:59 -0500

    Configure Saucelabs

---
+++ protractor/saucelabs.config.ts
@@ -0,0 +1,27 @@
+import { Config, browser } from 'protractor';
+import { reporter } from './helpers/reporter';
+
+export const config: Config = {
+  framework: 'jasmine',
+  specs: ['../test/**/*.spec.js'],
+  SELENIUM_PROMISE_MANAGER: false,
+  capabilities: {
+    name: 'UI Workshop',
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['--disable-popup-blocking', '--no-default-browser-check', '--window-size=800,600'],
+      prefs: { credentials_enable_service: false }
+    }
+  },
+  getPageTimeout: 30000,
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 120000
+  },
+  sauceUser: process.env.SAUCE_USERNAME,
+  sauceKey: process.env.SAUCE_ACCESS_KEY,
+  onPrepare: () => {
+    browser.ignoreSynchronization = true;
+    browser.manage().timeouts().implicitlyWait(3000);
+    reporter();
+  }
+};
