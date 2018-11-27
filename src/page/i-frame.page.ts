f6802d0d94524814c893aa77ec7d2c4fad54bd0e
Edwin Mantilla <hackpro.ems@gmail.com>
2018-11-27 11:53:58 -0500

    Working with IFrames

--- src/page/i-frame.page.ts
+++ src/page/i-frame.page.ts
@@ -1,17 +1,31 @@
 import { $, ElementFinder, browser, promise } from 'protractor';
 
 export class IFramePage {
-  private iframe1: ElementFinder;
+  private iFrame1: ElementFinder;
 
   constructor () {
-    this.iframe1 = $('#IF1');
+    this.iFrame1 = $('#IF1');
+  }
+
+  public async switchToFrame1(): Promise<void> {
+    await browser.switchTo().frame(this.iFrame1.getWebElement());
+  }
+
+  public async switchToMainPage(): Promise<void> {
+    await browser.switchTo().defaultContent();
   }
 
   public async getFrame1Height(): Promise<number> {
-    return Number(await this.iframe1.getAttribute('height'));
+    return Number(await this.iFrame1.getAttribute('height'));
   }
 
   public setFrame1Height(height: number): promise.Promise<void> {
-    return browser.executeScript(`arguments[0].height = ${height};`, this.iframe1);
+    return browser.
+      executeScript(`arguments[0].height = ${height};`, this.iFrame1.getWebElement());
+  }
+
+  public getPageTitle(): promise.Promise<string> {
+    return browser.
+      executeScript("return document.getElementsByTagName('title')[0].text");
   }
 }
--- test/i-frame.spec.ts
+++ test/i-frame.spec.ts
@@ -12,5 +12,15 @@ describe('when open Automation Practice with iFrames', () => {
     const newHeight = 900;
     await iFramePage.setFrame1Height(newHeight);
     await expect(await iFramePage.getFrame1Height()).toBe(newHeight);
+    await expect(await iFramePage.getPageTitle()).
+      toBe('TOOLSQA | Free QA Automation Tools Tutorials');
+
+    await iFramePage.switchToFrame1();
+    await expect(await iFramePage.getPageTitle()).
+      toBe('Demo Form for practicing Selenium Automation');
+
+    await iFramePage.switchToMainPage();
+    await expect(await iFramePage.getPageTitle()).
+      toBe('TOOLSQA | Free QA Automation Tools Tutorials');
   });
 });
